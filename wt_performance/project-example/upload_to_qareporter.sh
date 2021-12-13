fe9ba59deddf77c4fc576d71461bc563de9e4161
Pablo Diaz <pablo.diazcruz@telefonica.com>
2021-12-13 13:02:58 +0100

    test

--- wt_performance/project-example/upload_to_qareporter.sh
+++ wt_performance/project-example/upload_to_qareporter.sh
@@ -5,7 +5,7 @@
 
 # NON-UI JMeter execution mode using remote node injector (JMeter slave node)
 if [ "$QA_REPORTER_ENVIRONMENT" == "PRO" ]; then
-  QA_REPORTER_URL="http://qacdco.hi.inet/qaperformance-pre"
+  QA_REPORTER_URL="https://qacdco.d-consumer.com/qareporter"
 elif [ "$QA_REPORTER_ENVIRONMENT" == "PRE" ]; then
   QA_REPORTER_URL="http://qacdco.hi.inet/qaperformance-pre"
 else
