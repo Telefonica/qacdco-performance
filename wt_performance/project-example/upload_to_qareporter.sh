84b0cffe03084559a76ab6b8892fcb2a3d4aa692
Pablo Domínguez <pablo.dominguezsantana@telefonica.com>
2022-02-04 14:40:43 +0100

    fix: include version and cleanup

--- wt_performance/project-example/upload_to_qareporter.sh
+++ wt_performance/project-example/upload_to_qareporter.sh
@@ -3,6 +3,7 @@
 # Copyright Telefonica Digital
 # CDO QA Team <qacdo@telefonica.com>
 
+
 # NON-UI JMeter execution mode using remote node injector (JMeter slave node)
 if [ "$QA_REPORTER_ENVIRONMENT" == "PRO" ]; then
   QA_REPORTER_URL="https://qacdco.d-consumer.com/qareporter"
@@ -13,7 +14,8 @@ else
   exit -1
 fi
 
-echo "+++ Upload data to QA Reporter"
+echo -e "\n\n+++ Upload data to QA Reporter"
+
 PROJECT_OBJECT=$(curl -X GET "${QA_REPORTER_URL}/api/1.0/performance/projects/?name=${PERFORMANCE_PROJECT_NAME}")
 PROJECT_OBJECT=${PROJECT_OBJECT##[}
 PROJECT_OBJECT=${PROJECT_OBJECT%]}
@@ -22,12 +24,25 @@ if (( ${#PROJECT_OBJECT} )); then
   PROJECT_ID=$(echo "${PROJECT_OBJECT}" | awk -v FS="(project_id\":|,)" '{print $2}')
   EXECUTION_DATE=$(date +"%Y-%m-%d %H:%M")
 
-  EXECUTION_OBJECT=$(curl -X POST -d "project-id=${PROJECT_ID}&project-version=${PERFORMANCE_VERSION_NUMBER}&execution-name=autoexecution_${PERFORMANCE_VERSION_NUMBER}&execution-date=${EXECUTION_DATE}&module=${PERFORMANCE_PROJECT_MODULE}" ${QA_REPORTER_URL}/api/1.0/performance/executions/)
+  EXECUTION_OBJECT=$(curl -X POST \
+                          -d "name=autoexecution_${PERFORMANCE_VERSION_NUMBER}&\
+                              project-id=${PROJECT_ID}&\
+                              module=${PERFORMANCE_PROJECT_MODULE}&\
+                              version=${PERFORMANCE_VERSION_NUMBER}&\
+                              date=${EXECUTION_DATE}" \
+                          ${QA_REPORTER_URL}/api/1.0/performance/executions/)
+
   EXECUTION_ID=$(echo "${EXECUTION_OBJECT}" | awk -v FS="(execution_id\": |})" '{print $2}' | awk -F "," '{print $1}')
 
 
   cd ${PERFORMANCE_PROJECT_PATH}/${OUTPUT_FOLDER}
-  curl -X POST -F "module=${PERFORMANCE_PROJECT_MODULE}" -F "data=@jmeter/samples.csv" -F "project-id=${PROJECT_ID}" -F "execution-id=${EXECUTION_ID}" -F 'input-type=jmeter-csv' ${QA_REPORTER_URL}/api/1.0/performance/csv_loader
+  curl -X POST \
+       -F "module=${PERFORMANCE_PROJECT_MODULE}" \
+       -F "data=@jmeter/samples.csv" \
+       -F "project-id=${PROJECT_ID}" \
+       -F "execution-id=${EXECUTION_ID}" \
+       -F 'input-type=jmeter-csv'
+       ${QA_REPORTER_URL}/api/1.0/performance/csv_loader
 
   if [ "${PERFORMANCE_OBTAIN_HOST_MEASURES}" = "Yes" ]; then
 
