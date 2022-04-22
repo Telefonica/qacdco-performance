0f199d050cd0161258ccd8883258c4fd8c02bb1b
Pablo Díaz Cruz <pablo.diazcruz@telefonica.com>
2022-04-22 13:30:26 +0200

    Test/qareporter (#8)
    * test
    
    * test
    
    * test
    
    * fix: malformed param
    
    * fix: typo
    
    * fix: include module in execution creation
    
    * new script for testing purposes
    
    * fix: include version and cleanup
    
    * fix: typo
    
    * fix: typo
    
    * fix: typo
    
    * fix: style
    
    * fix: style
    
    * fix: style
    
    * fix: execution type
    
    * chore: include endpoint
    
    * fix: remove redundant parameters
    
    * fix: API path
    
    * fix: performance reporter url
    
    * increasing VM memory for Jmeter injectors
    
    * transactions name modifications
    
    * transactions name modifications
    
    * ignoring system mac files
    
    * removing useless upload_to_qa_reporter.sh script
    
    * new transaction to test Performance Reportes fixs
    
    Co-authored-by: Pablo Domínguez <pablo.dominguezsantana@telefonica.com>

--- wt_performance/project-example/upload_to_qareporter.sh
+++ wt_performance/project-example/upload_to_qareporter.sh
@@ -3,18 +3,14 @@
 # Copyright Telefonica Digital
 # CDO QA Team <qacdo@telefonica.com>
 
-# NON-UI JMeter execution mode using remote node injector (JMeter slave node)
-if [ "$QA_REPORTER_ENVIRONMENT" == "PRO" ]; then
-  QA_REPORTER_URL="https://qacdco.d-consumer.com/qareporter"
-elif [ "$QA_REPORTER_ENVIRONMENT" == "PRE" ]; then
-  QA_REPORTER_URL="https://qacdco.d-consumer.com/pre-qareporter"
-else
-  echo "Error, QA_REPORTER_URL must be PRE or PRO. Exiting..."
-  exit -1
-fi
+echo -e "\n\n+++ Upload data to QA Reporter"
 
-echo "+++ Upload data to QA Reporter"
-PROJECT_OBJECT=$(curl -X GET "${QA_REPORTER_URL}/api/1.0/performance/projects/?name=${PERFORMANCE_PROJECT_NAME}")
+case ${PERFORMANCE_REPORTER} in
+  PRE) QA_REPORTER_URL="http://qacdco.hi.inet/pre-performance/reporter" ;;
+  PROD) QA_REPORTER_URL="http://qacdco.hi.inet/performance/reporter" ;;
+esac
+
+PROJECT_OBJECT=$(curl -X GET "${QA_REPORTER_URL}/api/1.0/projects/?name=${PERFORMANCE_PROJECT_NAME}")
 PROJECT_OBJECT=${PROJECT_OBJECT##[}
 PROJECT_OBJECT=${PROJECT_OBJECT%]}
 
@@ -22,33 +18,68 @@ if (( ${#PROJECT_OBJECT} )); then
   PROJECT_ID=$(echo "${PROJECT_OBJECT}" | awk -v FS="(project_id\":|,)" '{print $2}')
   EXECUTION_DATE=$(date +"%Y-%m-%d %H:%M")
 
-  EXECUTION_OBJECT=$(curl -X POST -d "project-id=${PROJECT_ID}&project-version=${PERFORMANCE_VERSION_NUMBER}&execution-name=autoexecution_${PERFORMANCE_VERSION_NUMBER}&execution-date=${EXECUTION_DATE}" ${QA_REPORTER_URL}/api/1.0/performance/executions/)
-  EXECUTION_ID=$(echo "${EXECUTION_OBJECT}" | awk -v FS="(execution_id\":|})" '{print $2}')
+  EXECUTION_OBJECT=$(curl -X POST                                   \
+                          -F "project-id=${PROJECT_ID}"             \
+                          -F "module=${PERFORMANCE_MODULE_NAME}"    \
+                          -F "name=${PERFORMANCE_EXECUTION_NAME}"   \
+                          -F "type=${PERFORMANCE_EXECUTION_TYPE}"   \
+                          -F "version=${PERFORMANCE_VERSION_NAME}"  \
+                          -F "date=${EXECUTION_DATE}"               \
+                          ${QA_REPORTER_URL}/api/1.0/executions/)
+  EXECUTION_ID=$(echo "${EXECUTION_OBJECT}" | awk -v FS="(execution_id\": |})" '{print $2}' | awk -F "," '{print $1}')
 
 
   cd ${PERFORMANCE_PROJECT_PATH}/${OUTPUT_FOLDER}
-  curl -X POST -F "module=${PERFORMANCE_PROJECT_MODULE}" -F "data=@jmeter/samples.csv" -F "project-id=${PROJECT_ID}" -F "execution-id=${EXECUTION_ID}" -F 'input-type=jmeter-csv' ${QA_REPORTER_URL}/api/1.0/performance/csv_loader
+  curl -X POST                                 \
+       -F "data=@jmeter/samples.csv"           \
+       -F "execution-id=${EXECUTION_ID}"       \
+       -F 'input-type=jmeter-csv'              \
+       ${QA_REPORTER_URL}/api/1.0/csv_loader
 
   if [ "${PERFORMANCE_OBTAIN_HOST_MEASURES}" = "Yes" ]; then
 
       for file_cpu in "hosts/tmp_collectd_cpu_"*
       do
-        curl -X POST -F "module=${PERFORMANCE_PROJECT_MODULE}" -F "data=@$file_cpu" -F "project-id=${PROJECT_ID}" -F "execution-id=${EXECUTION_ID}" -F 'input-type=cpu-csv' ${QA_REPORTER_URL}/api/1.0/performance/csv_loader
+        curl -X POST                                    \
+             -F "module=${PERFORMANCE_PROJECT_MODULE}"  \
+             -F "data=@$file_cpu"                       \
+             -F "project-id=${PROJECT_ID}"              \
+             -F "execution-id=${EXECUTION_ID}"          \
+             -F 'input-type=cpu-csv'                    \
+             ${QA_REPORTER_URL}/api/1.0/csv_loader
       done
 
       for file_disk in "hosts/tmp_collectd_disk_"*
       do
-        curl -X POST -F "module=${PERFORMANCE_PROJECT_MODULE}" -F "data=@$file_disk" -F "project-id=${PROJECT_ID}" -F "execution-id=${EXECUTION_ID}" -F 'input-type=disk-csv' ${QA_REPORTER_URL}/api/1.0/performance/csv_loader
+        curl -X POST                                    \
+             -F "module=${PERFORMANCE_PROJECT_MODULE}"  \
+             -F "data=@$file_disk"                      \
+             -F "project-id=${PROJECT_ID}"              \
+             -F "execution-id=${EXECUTION_ID}"          \
+             -F 'input-type=disk-csv'                   \
+             ${QA_REPORTER_URL}/api/1.0/csv_loader
       done
 
       for file_mem in "hosts/tmp_collectd_memory_"*
       do
-        curl -X POST -F "module=${PERFORMANCE_PROJECT_MODULE}" -F "data=@$file_mem" -F "project-id=${PROJECT_ID}" -F "execution-id=${EXECUTION_ID}" -F 'input-type=memory-csv' ${QA_REPORTER_URL}/api/1.0/performance/csv_loader
+        curl -X POST                                    \
+             -F "module=${PERFORMANCE_PROJECT_MODULE}"  \
+             -F "data=@$file_mem"                       \
+             -F "project-id=${PROJECT_ID}"              \
+             -F "execution-id=${EXECUTION_ID}"          \
+             -F 'input-type=memory-csv'                 \
+             ${QA_REPORTER_URL}/api/1.0/csv_loader
       done
 
       for file_net in "hosts/tmp_collectd_network_"*
       do
-        curl -X POST -F "module=${PERFORMANCE_PROJECT_MODULE}" -F "data=@$file_net" -F "project-id=${PROJECT_ID}" -F "execution-id=${EXECUTION_ID}" -F 'input-type=network-csv' ${QA_REPORTER_URL}/api/1.0/performance/csv_loader
+        curl -X POST                                    \
+             -F "module=${PERFORMANCE_PROJECT_MODULE}"  \
+             -F "data=@$file_net"                       \
+             -F "project-id=${PROJECT_ID}"              \
+             -F "execution-id=${EXECUTION_ID}"          \
+             -F 'input-type=network-csv'                \
+             ${QA_REPORTER_URL}/api/1.0/csv_loader
       done
 
   else
